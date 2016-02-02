5e6a24edc4be9b5a6248996abfa4c13e7abfe312
Roberto Loja <robertoloja@gmail.com>
2016-02-01 23:54:50 -0500

    Finished Title Page function.

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from docx import Document
-from docx.shared import Inches
+import docx
 
-document = Document()
+document = docx.Document()
 
-document.add_heading('Title', 0)
+document.add_heading('\n\n\n\n\n\n\n\n\n\nTitle', 1).alignment = docx.\
+        enum.text.WD_ALIGN_PARAGRAPH.CENTER
 p = document.add_paragraph('Things and such \n')
 p.add_run('another thing').bold = True
 document.add_page_break()
